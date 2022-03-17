0916992bd70070d921759436b2bdb59db9f29160
Vanurina Masha <90695332+Redvence@users.noreply.github.com>
2022-03-17 22:26:44 +0300

    /

--- javascripts/js.js
+++ javascripts/js.js
@@ -1,6 +1,64 @@
+//кнопочки!! переключение экранов
 $(document).ready(function(){
   $(".basenokia").click(function(){
     $(".sectionmain").css("display","none");
-    $(".example").css("display","block");
+    $(".nokiasection").css("display","block");
   });
 });
+$(document).ready(function(){
+  $(".back").click(function(){
+    $(".nokiasection").css("display","none");
+    $(".sectionmain").css("display","block");
+  });
+});
+// нокиа!!
+let current = 1;
+      let msgCount = 4;
+        $(document).ready(function(){
+          $(".b4").click(function(){
+            if (current == 1){
+            $(".msg1, .msg3, .msg4").css("display","none");
+            $(".msg2").css("display","block");
+            current++;
+            }
+            else if (current == 2){
+            $(".msg1, .msg2, .msg4").css("display","none");
+            $(".msg3").css("display", "block");
+            current++;
+            }
+            else if(current ==3){
+            $(".msg1, .msg2, .msg3").css("display","none");
+            $(".msg4").css("display", "block");
+            current++;
+            }
+            else if (current == msgCount || current <= 0 || current > msgCount){
+            $(".msg1").css("display","block");
+            $(".msg2, .msg3, .msg4").css("display","none");
+            current = 1;
+            }
+          });
+        });
+        $(document).ready(function(){
+          $(".b5").click(function(){
+            if (current == 1 || current <= 0 || current > msgCount){
+            $(".msg4").css("display","block");
+            $(".msg1, .msg2, .msg3").css("display","none");
+            current = 4;
+            }
+            else if (current == 4){
+            $(".msg1, .msg2, .msg4").css("display","none");
+            $(".msg3").css("display","block");
+            current--;
+            }
+            else if (current == 2){
+            $(".msg2, .msg3, .msg4").css("display","none");
+            $(".msg1").css("display", "block");
+            current--;
+            }
+            else if (current == 3){
+            $(".msg1, .msg3, .msg4").css("display","none");
+            $(".msg2").css("display", "block");
+            current--;
+            }
+          });
+        });
